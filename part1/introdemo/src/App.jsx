bb69ce030a739f87d9e811ac8a3edb9744d89a18
Cláudia Carvalho <claudiacvlho@gmail.com>
2025-03-02 13:19:29 +0000

    feat: exercise folder

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -7,14 +7,20 @@ const Hello = (props) => {
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const friends = [ 'Peter', 'Maya']
 
   return (
     <div>
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <p>{friends}</p>
     </div>
   )
 }
